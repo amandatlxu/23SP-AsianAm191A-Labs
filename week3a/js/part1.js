c5e88ea10bbdb85892b25c423195685f4f71b2bb
amandatlxu <amandatlau@g.ucla.edu>
2023-06-21 21:19:57 -0700

    lab changes

---
+++ week3a/js/part1.js
@@ -0,0 +1,31 @@
+// declare the map and use the variables above
+const map = L.map('the_map').setView([34.0709,-118.444], 5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+addMarker(39.739235,-104.990250,'Denver, Colorado','want to visit!')
+addMarker(40.712776,-74.005974,'New York City, New York','want to visit soon!')
+addMarker(32.715736,-117.161087,'San Diego, California','Visited before, would like to return!')
+addMarker(29.951065,-90.071533,'New Orleans, Louisiana','hope to visit soon!')
+
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    createButtons(lat,lng,title);
+    return message
+}
+
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+    })
+    document.getElementById("contents").appendChild(newButton); //this adds the button to our page.
+}
---
+++ week3b/js/init.js
@@ -0,0 +1,48 @@
+
+// declare variables
+let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+
+// use the variables
+const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+addMarker(39.739235,-104.990250,'Denver, Colorado','want to visit!')
+addMarker(40.712776,-74.005974,'New York City, New York','want to visit soon!')
+addMarker(29.951065,-90.071533,'New Orleans, Louisiana','hope to visit soon!')
+
+// create a function to add markers
+function addMarker(lat,lng,title,message){
+    console.log(message)
+    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
+    return message
+}
+
+fetch("map.geojson")
+    .then(response => {
+        return response.json()
+    })
+    .then(data =>{
+        // Basic Leaflet method to add GeoJSON data
+        L.geoJSON(data, {
+                pointToLayer: (feature, latlng) => { 
+                    return L.circleMarker(latlng, {color: feature.properties.color})
+                }
+            }).bindPopup(layer => {
+                return layer.feature.properties.place;
+            }).addTo(map);
+    })
+
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button"); // adds a new button
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
+    })
+    document.getElementById("contents").appendChild(newButton); //this adds the button to our page.
+}
